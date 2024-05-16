269941eccee322bbe2c3df92fa0256db246fcd76
Stefymare <74431493+Stefymare@users.noreply.github.com>
2024-05-16 16:42:40 +0200

    Update the class for Global Timer

--- IWP/Assets/Scripts/GlobalTimer.cs
+++ IWP/Assets/Scripts/GlobalTimer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-public class Timer : MonoBehaviour
+public class GlobalTimer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     float elapsedTime;
