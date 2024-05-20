8774d59deac18d3b2e3f861486187a973ac8f841
yanshenpan <yanshen1236@gmail.com>
2024-05-20 20:08:57 +0200

    Fixed
    fixes

---
+++ IWP/Assets/Scripts/Countries.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countries : MonoBehaviour
+{
+
+[SerializeField]
+private float Assemblys;
+
+[SerializeField]
+private float Stores;
+
+[SerializeField]
+private float Storages;
+
+[SerializeField]
+private float Money;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
