0c06e39688b6f48dbb1567692f8ae6ec303a2728
SexyBeast42 <davakoeswoyo11@gmail.com>
2024-06-05 14:50:15 +0200

    building mode is done

--- IWP/Assets/Scripts/Building Mode/BuilidingManager.cs
+++ IWP/Assets/Scripts/Building Mode/BuilidingManager.cs
@@ -1,15 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-
-public enum BuildingType
-{
-    Warehouse,
-    Assembly,
-    Factory,
-    Shop
-}
+using UnityEngine.UI;
 
 public class BuilidingManager : MonoBehaviour
 {
@@ -18,29 +12,85 @@ public class BuilidingManager : MonoBehaviour
     private List<GameObject> _ownedBuildings = new List<GameObject>();
         
     public GameObject buildingPanelUI;
-    public GameObject buildingButtonUI;
+    public Image buildingButtonImage;
 
-    private bool _isInBuildingMode;
+    private GameObject _currentSpawnLocationChosen;
+    
+    private bool _isInBuildingMode = false;
 
-    void Update()
+    void Start()
     {
-        
+        ToggleBuildingPanelUI(false);
+        ToggleSpawnLocations(false);
+        buildingButtonImage.color = Color.gray;
     }
     
-    void FixedUpdate()
+    void Update()
     {
         if (_isInBuildingMode)
         {
-            
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectSpawnLocation();
+            }
         }
-        else
+    }
+
+    private void SelectSpawnLocation()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
         {
-            
+            SphereCollider sphereCollider = hit.collider.GetComponent<SphereCollider>();
+            if (sphereCollider != null && _spawnLocations.Contains(hit.collider.gameObject))
+            {
+                _currentSpawnLocationChosen = hit.collider.gameObject;
+                ToggleBuildingPanelUI(true);
+            }
         }
+    }
+
+    public void BuyBuilding(string type)
+    {
+        GameObject buildingToBuy = _buildings.FirstOrDefault(b => b.name == type);
+        
+        if (buildingToBuy != null)
+        {
+            Instantiate(buildingToBuy, _currentSpawnLocationChosen.transform.position, Quaternion.identity);
+            _ownedBuildings.Add(buildingToBuy);
+            _spawnLocations.Remove(_currentSpawnLocationChosen);
+            _currentSpawnLocationChosen.SetActive(false);
+            ToggleBuildingPanelUI(false);
+        }
+    }
+
+    private void ToggleSpawnLocations(bool show)
+    {
+        foreach (var spawnLocation in _spawnLocations)
+        {
+            spawnLocation.SetActive(show);
+        }
+    }
+
+    private void ToggleBuildingPanelUI(bool show)
+    {
+        buildingPanelUI.SetActive(show);
     }
 
     public void ToggleBuildingMode()
     {
         _isInBuildingMode = !_isInBuildingMode;
+        ToggleSpawnLocations(_isInBuildingMode);
+
+        if (_isInBuildingMode)
+        {
+            buildingButtonImage.color = Color.white;
+        }
+        else
+        {
+            buildingButtonImage.color = Color.gray;
+        }
     }
 }
