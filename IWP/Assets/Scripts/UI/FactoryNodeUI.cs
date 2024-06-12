8279cb8cd2364bb1f4f3e89f91bd18bc3bcd1c08
SexyBeast42 <davakoeswoyo11@gmail.com>
2024-06-12 18:54:34 +0200

    Store and factory finished

--- IWP/Assets/Scripts/UI/FactoryNodeUI.cs
+++ IWP/Assets/Scripts/UI/FactoryNodeUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -7,16 +8,38 @@ using UnityEngine.UI;
 
 public class FactoryNodeUI : BaseNodeUI
 {
+    public List<Sprite> productIcons = new List<Sprite>();
     public TMP_Text factoryInfomationText;
     public Image factoryProductIcon;
-
-    public void UpdateRefineryText(string productType)
+    
+    public void UpdateFactoryImage(ProductType productType)
     {
-        factoryInfomationText.text = $"The refinery is producing: {productType}";
+        Sprite sprite = productIcons.FirstOrDefault(a => a.name == productType.ToString());
+
+        if (sprite == null)
+        {
+            Debug.LogError($"The {sprite} is not valid. Entered in the wrong product type. Only KnifeBlade and KnifeHandle is valid.");
+            return;
+        }
+        
+        factoryProductIcon.sprite = sprite;
+        
+        UpdateFactoryText(productType.ToString());
     }
-
-    public void UpdateRefineryImage(Image productIcon)
+    
+    private void UpdateFactoryText(string productType)
     {
-        factoryProductIcon = productIcon;
+        string textCorrection = "Nothing";
+
+        if (productType == $"KnifeBlade")
+        {
+            textCorrection = $"Knife Blades";
+        }
+        else if (productType == $"KnifeHandle")
+        {
+            textCorrection = $"Knife Handles";
+        }
+        
+        factoryInfomationText.text = $"Producing: {textCorrection}";
     }
 }
