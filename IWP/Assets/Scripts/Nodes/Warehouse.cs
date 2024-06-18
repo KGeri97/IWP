b4f9b00d65ff56d662d7ce7eb7f333aaba746b4a
KGeri97 <kgeri999995@gmail.com>
2024-06-18 12:57:44 +0200

    More bugfixes

--- IWP/Assets/Scripts/Nodes/Warehouse.cs
+++ IWP/Assets/Scripts/Nodes/Warehouse.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Warehouse : Node
-{
-    private void Start() {
-        
+public class Warehouse : Node {
+
+    public override void Awake() {
+        base.Awake();
     }
 }
